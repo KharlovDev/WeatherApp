12de4a67a296a5758ed45e2c1cd3d3e1068073f3
KharlovDev <KharlovN@gmail.com>
2022-09-05 12:44:04 +0300

    Add Data Class

---
+++ app/src/main/java/com/example/ideaapp/data/DataClass.kt
@@ -0,0 +1,13 @@
+package com.example.ideaapp.data
+
+data class DayItem(
+    val city: String,
+    val time: String,
+    val condition: String,
+    val imageUrl: String,
+    val maxTemp: String,
+    val currentTemp: String,
+    val minTemp: String,
+    val hours: String
+
+)
