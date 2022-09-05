ec1542665281dd846c867cbbe6e81d51e9124be0
KharlovDev <KharlovN@gmail.com>
2022-09-05 11:43:08 +0300

    Project created

--- app/src/main/java/com/example/ideaapp/MainActivity.kt
+++ app/src/main/java/com/example/ideaapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.ideaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ideaapp.databinding.ActivityMainBinding
+import com.example.ideaapp.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance()).commit()
     }
 }
